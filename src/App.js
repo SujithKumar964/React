f8011e6a62cf7cb5fd8908df65159277ef5ffeb9
Sujith Kumar <boggavarapusujithkumar@gmail.com>
2023-01-25 19:54:08 +0530

    the components are cahnges

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './Home';
 import Login from './Login';
-import {BrowserRouter as Router ,Route , Routes} from 'react-router-dom';
+import {Route , Routes} from 'react-router-dom';
 
 function App() {
   return (
-      <Routes>
-        <Route exact path='/' element={Home}></Route>
-        <Route path='/login' element={Login}></Route>
-      </Routes>
+    <Routes>
+      <Route exact path="/" element={<Home/>}></Route>
+      <Route exact path="/Login" element={<Login/>}></Route>
+    </Routes>
   );
 }
 
